cae778569a9bc38c2909c02c06e436c89b117627
Harmony Teng <hteng1@lsu.edu>
2025-03-08 22:23:45 -0600

    update about fisrt two pages

--- lib/main.dart
+++ lib/main.dart
@@ -27,16 +27,27 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('LSU Scavenger Hunt')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Welcome to the LSU Scavenger Hunt",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
+      appBar: AppBar(title: const Text('Scavenger Hunt')),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/lsu1.jpg"),
+            fit: BoxFit.cover, // Adjusts the image to fill the screen
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Welcome to the LSU Scavenger Hunt",
+                style: TextStyle(
+                  fontSize: 70,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white, // Ensuring visibility over the image
+                ),
+                textAlign: TextAlign.center,
+              ),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
@@ -45,13 +56,49 @@ class WelcomeScreen extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => const IntroScreen()),
                 );
               },
-              child: const Text("Next"),
+              child: const Text("Begin",
+              style: TextStyle(
+                fontSize: 30,
+              ),),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Mapscreen()),
+                );
+              },
+              child: const Text("Pft Map",
+              style: TextStyle(
+                fontSize: 27,
+              ),),
             ),
           ],
         ),
       ),
+      )
     );
   }
+}
+
+//map
+class Mapscreen extends StatelessWidget{
+  const Mapscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('PFT Map')),
+      body: ListView(
+        children: [
+          Image.asset("assets/map1.png", fit: BoxFit.contain),
+          Image.asset("assets/map2.png", fit: BoxFit.contain),
+          Image.asset("assets/map3.png", fit: BoxFit.contain),
+        ],
+        ),
+    );
+}
 }
 
 // Intro Screen
@@ -61,14 +108,25 @@ class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Get Ready!')),
-      body: Center(
+      appBar: AppBar(title: const Text('Scavenger Hunt of PFT')),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/pft1.jpg"),
+            fit: BoxFit.cover, // Adjusts the image to fill the screen
+          ),
+        ),
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
               "Time to see how well you know the PFT!",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                fontSize: 60, 
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                ),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 20),
@@ -79,11 +137,12 @@ class IntroScreen extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => const QuizScreen(questionIndex: 0)),
                 );
               },
-              child: const Text("Play"),
+              child: const Text("Play", style: TextStyle(fontSize:25)),
             ),
           ],
         ),
       ),
+      )
     );
   }
 }
@@ -260,94 +319,6 @@ class CongratsScreen extends StatelessWidget {
           textAlign: TextAlign.center,
         ),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
